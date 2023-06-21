78dd1568a4c990369a05af2e1ce15966e4d6c344
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2023-06-22 00:39:52 +0700

    fix: image bot versions

--- src/imagebot.py
+++ src/imagebot.py
@@ -5,7 +5,7 @@ class Imagebot:
     def __init__(self, token) -> None:
         self.api = replicate.Client(api_token=token)
         self.model = self.api.models.get("stability-ai/stable-diffusion")
-        self.version = self.versions.get("db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
+        self.version = self.model.versions.get("db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf")
 
     def generate_image(self, query):
         output_url = self.version.predict(prompt=query)[0]
