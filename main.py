f5ddcfcc28ccdcd70bf05e89399e4c3fac28d114
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2023-07-10 15:36:41 +0700

    style: remove main

--- main.py
+++ main.py
@@ -5,27 +5,28 @@ from src.chatbot import Chatbot
 from src.imagebot import Imagebot
 
 
-def main():
-    intents = discord.Intents.default()
-    # intents.message_content = True
-    client = commands.Bot(command_prefix="!", intents=intents)
-    chatbot = Chatbot(os.environ["poe_api_token"])
-    imagebot = Imagebot(os.environ["replicate_api_token"])
-
-    @client.command(name="q")
-    async def q(ctx, *query):
-        query = " ".join(query)
-        # channel = client.get_channel(os.environ["channel_id"])
-        await ctx.send(f"{chatbot.query_from_api(query=query)}")
-
-    @client.command(name="gen")
-    async def gen(ctx, *query):
-        query = " ".join(query)
-        # channel = client.get_channel(os.environ["channel_id"])
-        await ctx.send(imagebot.generate_image(query=query))
-
-    client.run(os.environ["discord_token"])
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="/", intents=intents)
+chatbot = Chatbot(os.environ["poe_api_token"])
+imagebot = Imagebot(os.environ["replicate_api_token"])
 
 
-if __name__ == "__main__":
-    main()
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+
+@bot.command(name="q")
+async def q(ctx, *query):
+    query = " ".join(query)
+    await ctx.send(f"{chatbot.query_from_api(query=query)}")
+
+
+@bot.command(name="gen")
+async def gen(ctx, *query):
+    query = " ".join(query)
+    await ctx.send(imagebot.generate_image(query=query))
+
+
+bot.run(os.environ["discord_token"])
