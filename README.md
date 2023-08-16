2d638d8dc8a91d9b7593eedcfdb62767d951d9ce
ndhieunguyen <hieunguyen.kientuong@gmail.com>
2023-08-16 20:34:04 +0700

    add: gradio bot

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+---
+title: echo-chatbot
+app_file: .\test_gradio.py
+sdk: gradio
+sdk_version: 3.40.1
+---
 # discord_bot
---
+++ app.py
@@ -0,0 +1,8 @@
+import gradio as gr
+
+
+def slow_echo(message, history):
+    return message
+
+
+demo = gr.ChatInterface(slow_echo).queue().launch()
